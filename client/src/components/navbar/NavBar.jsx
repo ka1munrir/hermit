0edfc1c51c85f5735e084c201fe04e740fb5c0c2
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-10-17 13:54:13 -0600

    made 2 versions of navbar and added some css

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import'../navbar/NavBar.css'
 
 function NavBar() {
-  return (
-    <div>NavBar</div>
+  const loggedOutNavBar = (
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </nav>
+    </header>
   )
+  const loggedInNavBar = (
+    <header>
+      <nav>
+        <NavLink to='/'>Quests</NavLink>
+        <NavLink to='/'>Guilds</NavLink>
+        <NavLink to='/'>Profile</NavLink>
+        <NavLink to='/'>Log Out</NavLink>
+      </nav>
+    </header>
+  )
+  return loggedInNavBar
+
 }
 
 export default NavBar
