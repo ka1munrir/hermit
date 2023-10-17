0066f19258c9f252489f53ba58800464ffb25732
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-10-17 14:56:25 -0600

    created log in

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <h1 className='Title-container'>Welcome to Hermit</h1>
       <p className='Description-container'>
         Do you struggle with social anxiety and find it challenging to engage in social situations?
-        Hermit is here to help. We understand the difficulties that social anxiety can present, and our goal is to provide you with the tools and support you need to overcome it in a fun and light hearted way.
+        Hermit is here to help all in the Denver area. We understand the difficulties that social anxiety can present, and our goal is to provide you with the tools and support you need to overcome it in a fun and fantastical way.
       </p>
 
     </div>
--- client/src/pages/login/LogIn.jsx
+++ client/src/pages/login/LogIn.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState } from "react";
 
-function LogIn() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userObject = { "username": username, "password": password }
+
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userObject)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response error");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log("error", error.message);
+      });
+  };
+
   return (
-    <div>LogIn</div>
-  )
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default LogIn
+export default Login;
