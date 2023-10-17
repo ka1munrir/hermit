43dbae679b54cfc5eb6c41f492c267576870fc9f
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-10-17 12:52:41 -0600

    Worked on Homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='Home-container'>
+
+      <h1 className='Title-container'>Welcome to Hermit</h1>
+      <p className='Description-container'>
+        Do you struggle with social anxiety and find it challenging to engage in social situations?
+        Hermit is here to help. We understand the difficulties that social anxiety can present, and our goal is to provide you with the tools and support you need to overcome it in a fun and light hearted way.
+      </p>
+
+    </div>
   )
 }
 
