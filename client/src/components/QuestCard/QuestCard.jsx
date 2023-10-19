a809809614b86806b97a782970230bc895cb8c29
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-10-19 14:20:35 -0600

    worked on userquest popup

--- client/src/components/QuestCard/QuestCard.jsx
+++ client/src/components/QuestCard/QuestCard.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import Popup from 'reactjs-popup';
+
 import './QuestCard.css'
 
 function QuestCard({quest}) {
   const { title, imgURL } = quest
   return (
-    <div className='questCard-div'>
-      {/* <img src={imgURL} alt={`${title} Image`} /> */}
-      <div style={{backgroundImage: `url(\"${imgURL}\")`}} className='image'></div>
-      <h1>{title}</h1>
-    </div>
+      <Popup trigger=
+          {
+            <div className='questCard-div'>
+              <div style={{backgroundImage: `url(\"${imgURL}\")`}} className='image'></div>
+              <h1>{title}</h1>
+            </div>
+          } 
+          modal nested>
+          {
+              close => (
+                <div className='questCardPopUp-div'>
+                <div style={{backgroundImage: `url(\"${imgURL}\")`}} className='imagePopUp'></div>
+                <h1>{title}</h1>
+              </div>
+              )
+          }
+      </Popup>
   )
 }
 
