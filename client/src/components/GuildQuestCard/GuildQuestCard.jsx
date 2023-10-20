34eb7202e80508e6f1a67de440a75d34bdeb6bfd
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-10-20 11:17:44 -0600

    add to user quests fuckshitality

--- client/src/components/GuildQuestCard/GuildQuestCard.jsx
+++ client/src/components/GuildQuestCard/GuildQuestCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Popup from 'reactjs-popup';
 import '../GuildQuestCard/GuildQuestCard.css'
+import useUserStore from "../../hooks/userStore";
 
 function QuestCard({ quest }) {
-  const { description, title, genre, difficulty, city, age_restriction, imgURL } = quest
-
+  const { id, description, title, genre, difficulty, city, age_restriction, imgURL } = quest
+  const { user } = useUserStore()
   return (
     <Popup trigger=
       {
@@ -16,16 +17,48 @@ function QuestCard({ quest }) {
       modal nested>
       {
         close => (
-          <div className='popup-div'>
-            <div className='questCardPopUp-div'>
-              <div style={{ backgroundImage: `url(\"${imgURL}\")` }} className='imagePopUp'></div>
-              <p>{title}</p>
-              <p>{description}</p>
-              <p>{genre}</p>
-              <p>{difficulty}</p>
-              <p>{city}</p>
-              <p>{age_restriction}</p>
+          <div id='guildQuestCardPopUp-div'>
+            <div style={{ backgroundImage: `url(\"${title}\")` }} id='userQuestCardImagePopUp'></div>
+            <h1>{title}</h1>
+            <div id='topGuildQuestDetails'>
+              <p className='guildQuestDetails'>{`Age: ${age_restriction}+`}</p>
+              <p className='guildQuestDetails'>{`Difficulty: ${difficulty}`}</p>
+              <p className='guildQuestDetails'>{`City: ${city}`}</p>
             </div>
+            <p className='guildQuestDetails'>{`Guild: ${genre}`}</p>
+            <p className='guildQuestDetails'>{description}</p>
+            <button onClick={() => {
+              fetch('/api/userquests', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  "user_id": user.id,
+                  "quest_id": id,
+                  "last_given": 0,
+                  "status": 'Not Started'
+                })
+              })
+                .then(response => {
+                  if (!response.ok) {
+                    throw new Error("Network response error");
+                  }
+                  return response.json();
+                })
+                .then(data => {
+                  console.log(data);
+                })
+                .catch(error => {
+                  console.log("error", error.message);
+                });
+            }}>Embark on the quest?</button>
+            {/* <select name="status" id="statusSelector" defaultValue={status} onChange={(e) => changeStatus(e.target.value)}>
+              <option value="Not Started">Not Started</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Completed">Completed</option>
+              <option value="Forsaken">Forsaken</option>
+            </select> */}
           </div>
         )
       }
